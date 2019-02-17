9de47a15adf6414e23b940e369ab933a1045e145
Michael L Perry <michael@qedcode.com>
2019-02-16 20:44:16 -0600

    Initialize a new Jinaga container for each test.

--- test/spec.ts
+++ test/spec.ts
@@ -1,8 +1,6 @@
 import { expect } from "chai";
-import { JinagaBrowser } from "jinaga";
+import { JinagaBrowser, Jinaga } from "jinaga";
 import { StateManager, collection, fixed } from "../src/index";
-
-const j = JinagaBrowser.create({});
 
 class Root {
     static Type = 'Application.Root';
@@ -19,11 +17,11 @@ class Item {
 
     constructor(
         public root: Root,
-        public createdAt: Date
+        public createdAt: Date | string
     ) { }
 
     static inRoot(r: Root) {
-        return j.match(<Item>{
+        return Jinaga.match(<Item>{
             type: Item.Type,
             root: r
         });
@@ -53,10 +51,11 @@ class Application {
         this.state = state;
     }
 
-    componentDidMount() {
+    componentDidMount(j: Jinaga) {
         const root = new Root('home');
         this.watch = StateManager.forComponent(this, root, j, [
-            collection('items', j.for(Item.inRoot), i => i.key, [
+            collection('items', Jinaga.for(Item.inRoot), i => i.key, [
+                fixed('key', i => j.hash(i)),
                 fixed('fact', i => i)
             ])
         ]);
@@ -70,18 +69,35 @@ class Application {
 }
 
 describe('Application State', () => {
-    it('should watch', () => {
-        const application = new Application();
-        application.componentDidMount();
+    var j: Jinaga;
+    var application: Application;
+
+    beforeEach(() => {
+        j = JinagaBrowser.create({});
+        application = new Application();
+        application.componentDidMount(j);
+    });
+
+    afterEach(() => {
         application.componentWillUnmount();
+    })
+
+    it('should watch', () => {
         expect(application.state).to.not.be.null;
     });
 
     it('should add to a collection', async () => {
-        const application = new Application();
-        application.componentDidMount();
         await j.fact(new Item(new Root('home'), new Date()));
         expect(application.state.items.length).to.equal(1);
-        application.componentWillUnmount();
+    });
+
+    it('should resolve the fact', async () => {
+        await j.fact(new Item(new Root('home'), new Date()));
+        expect(application.state.items[0].fact.type).to.equal(Item.Type);
+    });
+
+    it('should resolve the key', async () => {
+        const item = await j.fact(new Item(new Root('home'), new Date()));
+        expect(application.state.items[0].key).to.equal(j.hash(item));
     });
 });
