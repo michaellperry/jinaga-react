057165174317f7a5ca8ebc6e276b6fa6dd373568
Michael L Perry <michael@qedcode.com>
2019-02-21 16:28:32 -0600

    Test that unmounting stops watching.

--- test/hooks.ts
+++ test/hooks.ts
@@ -16,25 +16,63 @@ describe('useJinaga', () => {
     it('should initialize state', async () => {
         const j = JinagaBrowser.create({});
         const root = await j.fact(new Root('home'));
-        const { result } = renderHook(() => useJinaga<Root, ApplicationState>(root,j, [
+        const { result, unmount } = renderHook(() => useJinaga<Root, ApplicationState>(root,j, [
             property('name', j.for(Name.inRoot), n => n.value, 'initial name')
         ]));
 
         expect(result.current.name).to.equal('initial name');
+
+        unmount();
     });
 
     it('should initialize children', async () => {
         const j = JinagaBrowser.create({});
         const root = await j.fact(new Root('home'));
-        const { result } = renderHook(() => useJinaga<Root, ApplicationState>(root,j, [
+        const { result, unmount } = renderHook(() => useJinaga<Root, ApplicationState>(root,j, [
             collection('items', j.for(Item.inRoot), i => i.key, [
                 field('key', i => j.hash(i))
             ])
         ]));
+
+        expect(result.current.items.length).to.equal(0);
+
         await j.fact(new Item(root, new Date()));
 
         expect(result.current.items.length).to.equal(1);
         expect(result.current.items[0].key).to.not.be.null;
         expect(result.current.items[0].key).to.not.equal('');
+
+        unmount();
+    });
+
+    it('should update a property', async () => {
+        const j = JinagaBrowser.create({});
+        const root = await j.fact(new Root('home'));
+        const { result, unmount } = renderHook(() => useJinaga<Root, ApplicationState>(root,j, [
+            property('name', j.for(Name.inRoot), n => n.value, '')
+        ]));
+
+        const first = await j.fact(new Name(root, 'Bandersnatch', []));
+        expect(result.current.name).to.equal('Bandersnatch');
+
+        await j.fact(new Name(root, 'Jabberwocky', [first]));
+        expect(result.current.name).to.equal('Jabberwocky');
+
+        unmount();
+    });
+
+    it('should stop watching when unmounted', async () => {
+        const j = JinagaBrowser.create({});
+        const root = await j.fact(new Root('home'));
+        const { result, unmount } = renderHook(() => useJinaga<Root, ApplicationState>(root,j, [
+            property('name', j.for(Name.inRoot), n => n.value, '')
+        ]));
+
+        const first = await j.fact(new Name(root, 'Bandersnatch', []));
+        expect(result.current.name).to.equal('Bandersnatch');
+
+        unmount();
+        await j.fact(new Name(root, 'Jabberwocky', [first]));
+        expect(result.current.name).to.equal('Bandersnatch');
     });
 });
--- types/react-hooks-testing-library.d.ts
+++ types/react-hooks-testing-library.d.ts
@@ -1,3 +1,6 @@
 declare module 'react-hooks-testing-library';
 
-export function renderHook<T>(render: () => T) : { result: { current: T } };
+export function renderHook<T>(render: () => T) : {
+    result: { current: T },
+    unmount: () => void
+};
