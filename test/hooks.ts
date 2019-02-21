aa681c23ab6b0794cac2f757807043ed066122e2
Michael L Perry <michael@qedcode.com>
2019-02-21 14:50:22 -0600

    Test initializing children in hook.

--- test/hooks.ts
+++ test/hooks.ts
@@ -1,10 +1,10 @@
 import { expect } from "chai";
 import { JinagaBrowser } from "jinaga";
-import { property, useJinaga } from "../src/index";
-import { Name, Root } from "./model";
-import { ApplicationState } from "./viewModel";
-import { renderHook } from "react-hooks-testing-library";
 import { JSDOM } from "jsdom";
+import { renderHook } from "react-hooks-testing-library";
+import { collection, field, property, useJinaga } from "../src/index";
+import { Item, Name, Root } from "./model";
+import { ApplicationState } from "./viewModel";
 
 const { window } = new JSDOM('<!doctype html><html><body></body></html>');
 // @ts-ignore
@@ -13,7 +13,7 @@ global['document'] = window.document;
 global['window'] = window;
 
 describe('useJinaga', () => {
-    it('should initialize state', async () =>{
+    it('should initialize state', async () => {
         const j = JinagaBrowser.create({});
         const root = await j.fact(new Root('home'));
         const { result } = renderHook(() => useJinaga<Root, ApplicationState>(root,j, [
@@ -21,5 +21,20 @@ describe('useJinaga', () => {
         ]));
 
         expect(result.current.name).to.equal('initial name');
+    });
+
+    it('should initialize children', async () => {
+        const j = JinagaBrowser.create({});
+        const root = await j.fact(new Root('home'));
+        const { result } = renderHook(() => useJinaga<Root, ApplicationState>(root,j, [
+            collection('items', j.for(Item.inRoot), i => i.key, [
+                field('key', i => j.hash(i))
+            ])
+        ]));
+        await j.fact(new Item(root, new Date()));
+
+        expect(result.current.items.length).to.equal(1);
+        expect(result.current.items[0].key).to.not.be.null;
+        expect(result.current.items[0].key).to.not.equal('');
     });
 });
